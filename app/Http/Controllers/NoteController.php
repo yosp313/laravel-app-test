140d5dbe54cdd765aad39b67d72fff2c3fde7778
yosp313 <147242847+yosp313@users.noreply.github.com>
2024-06-26 04:19:40 +0300

    finished the put and delete routes

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -11,15 +11,58 @@ class NoteController extends Controller
     {
         $notes = Note::all();
 
-        return $notes;
+        return response()->json($notes);
     }
 
     public function store(Request $request)
     {
+        $request->validate([
+            'title' => 'required|string',
+            'content' => 'required|string',
+        ]);
+
         $note = new Note;
         $note->title = $request->title;
         $note->content = $request->content;
 
         $note->save();
+
+        return response()->json($note, 201);
+    }
+
+    public function show(string $id)
+    {
+        $note = Note::query()->where("id", $id)->first();
+
+        if ($note == null) {
+            abort(404);
+        }
+
+        return response()->json($note);
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'content' => '|string',
+        ]);
+
+        $note = Note::query()->where("id", $id)->first();
+        $note->title = $request->title;
+        $note->content = $request->content;
+
+        $note->update();
+
+        return response()->json($note, 204);
+    }
+
+    public function destroy(string $id)
+    {
+        $note = Note::query()->where("id", $id)->first();
+
+        $note->delete();
+
+        return response()->json($note, 204);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ Route::get("/hola", function () {
 
 Route::post("/api/notes", [NoteController::class, 'store'])->name("notes.store");
 Route::get("/api/notes", [NoteController::class, 'index'])->name("notes.index");
+Route::get("/api/notes/{id}", [NoteController::class, 'show'])->name("notes.show");
+Route::put("/api/notes/{id}", [NoteController::class, 'update'])->name("notes.update");
+Route::delete("/api/notes/{id}", [NoteController::class, 'destroy'])->name("notes.destroy");
+
+
+
 Route::get("/api/csrf", function () {
     return response()->json(["csrf_token" => csrf_token()]);
 });
