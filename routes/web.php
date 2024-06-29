80c206fbfbdb23fd12efd21d99d8472a92825620
Youssef Ayman <yosp31@gmail.com>
2024-06-29 19:51:20 +0300

    Implemented a logging middleware for the index and api/notes route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\LoggingMiddleWare;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(LoggingMiddleWare::class);
 
 Route::get("/hola", function () {
     return "Hola Amigo!";
 });
 
 Route::post("/api/notes", [NoteController::class, 'store'])->name("notes.store");
-Route::get("/api/notes", [NoteController::class, 'index'])->name("notes.index");
+Route::get("/api/notes", [NoteController::class, 'index'])->name("notes.index")->middleware(LoggingMiddleWare::class);
 Route::get("/api/notes/{id}", [NoteController::class, 'show'])->name("notes.show");
 Route::put("/api/notes/{id}", [NoteController::class, 'update'])->name("notes.update");
 Route::delete("/api/notes/{id}", [NoteController::class, 'destroy'])->name("notes.destroy");
